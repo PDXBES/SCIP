a0e7a6eaec5724746d36a5cd8f18e4010f82b68b
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-03 13:32:25 -0700

    SCIPDatabase: Added hansen_area field to BASINS

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_FILL_LOOKUPS.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_FILL_LOOKUPS.proc.sql
@@ -15,165 +15,165 @@ BEGIN
 
   TRUNCATE TABLE [SCIP].[dbo].[BASINS];
   INSERT INTO BASINS VALUES
-    ('ALB','C','ALBINA');
+    ('ALB','C','ALBINA','Albina');
   INSERT INTO BASINS VALUES
-    ('ALD','C','ALDER');
+    ('ALD','C','ALDER','Alder');
   INSERT INTO BASINS VALUES
-    ('BAY','C','BAYARD');
+    ('BAY','C','BAYARD','Bayard');
   INSERT INTO BASINS VALUES
-    ('BCC','T','BCC');
+    ('BCC','T','BCC','Balch Consolidation Conduit');
   INSERT INTO BASINS VALUES
-    ('BAL','C','BALCH COMB');
+    ('BAL','C','BALCH COMB','Balch');
   INSERT INTO BASINS VALUES
-    ('BEE','C','BEECH ESSX');
+    ('BEE','C','BEECH ESSX','Beech/Essex');
   INSERT INTO BASINS VALUES
-    ('CAL','C','CALIFORNIA');
+    ('CAL','C','CALIFORNIA','California');
   INSERT INTO BASINS VALUES
-    ('CAR','C','CAROLINA');
+    ('CAR','C','CAROLINA','Carolina');
   INSERT INTO BASINS VALUES
-    ('CBI','I','COL BL INT');
+    ('CBI','I','COL BL INT','Columbia Blvd Interceptor');
   INSERT INTO BASINS VALUES
-    ('CBX','S','CBD DRAIN');
+    ('CBX','S','CBD DRAIN','CBD Drainage');
   INSERT INTO BASINS VALUES
-    ('CBD','C','CBD');
+    ('CBD','C','CBD','Central Business District');
   INSERT INTO BASINS VALUES
-    ('CHA','C','CHAUTAUQUA');
+    ('CHA','C','CHAUTAUQUA','Chautauqua');
   INSERT INTO BASINS VALUES
-    ('COL','S','COLUMBIA');
+    ('COL','S','COLUMBIA','Columbia');
   INSERT INTO BASINS VALUES
-    ('CSC','T','CSCC');
+    ('CSC','T','CSCC','CSCC');
   INSERT INTO BASINS VALUES
-    ('DIV','C','DIVISION');
+    ('DIV','C','DIVISION','Division');
   INSERT INTO BASINS VALUES
-    ('ECI','I','EC INT');
+    ('ECI','I','EC INT','East-Central Interceptor');
   INSERT INTO BASINS VALUES
-    ('EST','T','EST');
+    ('EST','T','EST','ESCSO Tunnel');
   INSERT INTO BASINS VALUES
-    ('FAN','S','FANNO SAN');
+    ('FAN','S','FANNO SAN','Fanno');
   INSERT INTO BASINS VALUES
-    ('FAI','I','FANNO INT');
+    ('FAI','I','FANNO INT','Fanno Interceptor');
   INSERT INTO BASINS VALUES
-    ('FEN','C','FENWICK');
+    ('FEN','C','FENWICK','Fenwick');
   INSERT INTO BASINS VALUES
-    ('FIS','C','FISKE');
+    ('FIS','C','FISKE','Fiske');
   INSERT INTO BASINS VALUES
-    ('FRE','C','FREMONT');
+    ('FRE','C','FREMONT','Fremont');
   INSERT INTO BASINS VALUES
-    ('GHR','S','GLEN HARBR');
+    ('GHR','S','GLEN HARBR','Glen Harbor');
   INSERT INTO BASINS VALUES
-    ('GUI','S','GUILDS LK');
+    ('GUI','S','GUILDS LK','Guilds Lake');
   INSERT INTO BASINS VALUES
-    ('HAY','S','HAYDEN SAN');
+    ('HAY','S','HAYDEN SAN','Hayden Island');
   INSERT INTO BASINS VALUES
-    ('HOL','C','HOLLADAY');
+    ('HOL','C','HOLLADAY','Holladay');
   INSERT INTO BASINS VALUES
-    ('INS','C','INSLEY');
+    ('INS','C','INSLEY','Insley');
   INSERT INTO BASINS VALUES
-    ('INV','S','INVERNESS');
+    ('INV','S','INVERNESS','Inverness');
   INSERT INTO BASINS VALUES
-    ('JCK','S','JOHNCK SAN');
+    ('JCK','S','JOHNCK SAN','Johnson Creek');
   INSERT INTO BASINS VALUES
-    ('JFX','C','JEFFERSON');
+    ('JFX','C','JEFFERSON','Jefferson');
   INSERT INTO BASINS VALUES
-    ('KEN','C','KENTON');
+    ('KEN','C','KENTON','Kenton');
   INSERT INTO BASINS VALUES
-    ('LE1','C','LENTS 1');
+    ('LE1','C','LENTS 1','Western Lents');
   INSERT INTO BASINS VALUES
-    ('LE2','C','LENTS 2');
+    ('LE2','C','LENTS 2','Lents');
   INSERT INTO BASINS VALUES
-    ('LIN','C','LINNTON');
+    ('LIN','C','LINNTON','Linnton');
   INSERT INTO BASINS VALUES
-    ('LII','I','LINNTN INT');
+    ('LII','I','LINNTN INT','Linnton Interceptor');
   INSERT INTO BASINS VALUES
-    ('LOW','C','LOWELL');
+    ('LOW','C','LOWELL','Lowell');
   INSERT INTO BASINS VALUES
-    ('LOI','I','LOMBRD INT');
+    ('LOI','I','LOMBRD INT','Lombard Interceptor');
   INSERT INTO BASINS VALUES
-    ('MET','S','METZGER');
+    ('MET','S','METZGER','Metzger');
   INSERT INTO BASINS VALUES
-    ('MIL','C','MILL');
+    ('MIL','C','MILL','Mill');
   INSERT INTO BASINS VALUES
-    ('MIS','S','MILL SAN');
+    ('MIS','S','MILL SAN','Mill Sanitary');
   INSERT INTO BASINS VALUES
-    ('N13','C','13TH AVE');
+    ('N13','C','13TH AVE','NE 13th Avenue');
   INSERT INTO BASINS VALUES
-    ('NIC','C','NICOLAI');
+    ('NIC','C','NICOLAI','Nicolai');
   INSERT INTO BASINS VALUES
-    ('NUL','A',' ');
+    ('NUL','A',' ','Null');
   INSERT INTO BASINS VALUES
-    ('ABA','A','ABANDONED');
+    ('ABA','A','ABANDONED','Abandoned');
   INSERT INTO BASINS VALUES
-    ('OAK','C','OAK');
+    ('OAK','C','OAK','Oak');
   INSERT INTO BASINS VALUES
-    ('ORE','C','OREGONIAN');
+    ('ORE','C','OREGONIAN','Oregonian');
   INSERT INTO BASINS VALUES
-    ('OSW','C','OSWEGO');
+    ('OSW','C','OSWEGO','Oswego');
   INSERT INTO BASINS VALUES
-    ('PE1','S','PEN DRA 1');
+    ('PE1','S','PEN DRA 1','Peninsula District 1');
   INSERT INTO BASINS VALUES
-    ('PRG','S','PEN/RIVER');
+    ('PRG','S','PEN/RIVER','Peninsula/Rivergate');
   INSERT INTO BASINS VALUES
-    ('RGI','I','RIVERG INT');
+    ('RGI','I','RIVERG INT','Rivergate Interceptor');
   INSERT INTO BASINS VALUES
-    ('RVW','S','RIVERVIEW');
+    ('RVW','S','RIVERVIEW','Riverview');
   INSERT INTO BASINS VALUES
-    ('RIV','C','RIVERS COM');
+    ('RIV','C','RIVERS COM','Riverside');
   INSERT INTO BASINS VALUES
-    ('STJ','C','STJOHN COM');
+    ('STJ','C','STJOHN COM','St. Johns');
   INSERT INTO BASINS VALUES
-    ('SEL','C','SELLWOOD');
+    ('SEL','C','SELLWOOD','Sellwood');
   INSERT INTO BASINS VALUES
-    ('SHE','C','SHERIDAN');
+    ('SHE','C','SHERIDAN','Sheridan');
   INSERT INTO BASINS VALUES
-    ('SJI','I','STJOHN INT');
+    ('SJI','I','STJOHN INT','St. Johns Interceptor');
   INSERT INTO BASINS VALUES
-    ('SKE','S','SKYL E SAN');
+    ('SKE','S','SKYL E SAN','Skyline East');
   INSERT INTO BASINS VALUES
-    ('SKW','S','SKYL W SAN');
+    ('SKW','S','SKYL W SAN','Skyline West');
   INSERT INTO BASINS VALUES
-    ('SKS','A','SKYL E DRA');
+    ('SKS','A','SKYL E DRA','Skyline East Drainage');
   INSERT INTO BASINS VALUES
-    ('SER','I','SE REL INT');
+    ('SER','I','SE REL INT','SE Relieving Interceptor');
   INSERT INTO BASINS VALUES
-    ('SEI','I','SE INT');
+    ('SEI','I','SE INT','SE Interceptor');
   INSERT INTO BASINS VALUES
-    ('STA','C','STARK');
+    ('STA','C','STARK','Stark');
   INSERT INTO BASINS VALUES
-    ('SWI','I','SW INT');
+    ('SWI','I','SW INT','SW Interceptor');
   INSERT INTO BASINS VALUES
-    ('SUL','C','SULLIVAN');
+    ('SUL','C','SULLIVAN','Sullivan');
   INSERT INTO BASINS VALUES
-    ('SWW','S','SOUTHWEST');
+    ('SWW','S','SOUTHWEST','Southwest');
   INSERT INTO BASINS VALUES
-    ('SWP','I','SWPI');
+    ('SWP','I','SWPI','SW Parallel Interceptor');
   INSERT INTO BASINS VALUES
-    ('TGA','C','TAGGART A');
+    ('TGA','C','TAGGART A','Taggart A');
   INSERT INTO BASINS VALUES
-    ('TGB','C','TAGGART B');
+    ('TGB','C','TAGGART B','Taggart B');
   INSERT INTO BASINS VALUES
-    ('TGC','C','TAGGART C');
+    ('TGC','C','TAGGART C','Taggart C');
   INSERT INTO BASINS VALUES
-    ('TGD','C','TAGGART D');
+    ('TGD','C','TAGGART D','Taggart D');
   INSERT INTO BASINS VALUES
-    ('TNA','C','TANNER A');
+    ('TNA','C','TANNER A','Tanner A');
   INSERT INTO BASINS VALUES
-    ('TNB','C','TANNER B');
+    ('TNB','C','TANNER B','Tanner B');
   INSERT INTO BASINS VALUES
-    ('TRI','I','TRYON INT');
+    ('TRI','I','TRYON INT','Tryon Interceptor');
   INSERT INTO BASINS VALUES
-    ('TRY','S','TRYON SAN');
+    ('TRY','S','TRYON SAN','Tryon');
   INSERT INTO BASINS VALUES
-    ('VAN','C','VANCOUVER');
+    ('VAN','C','VANCOUVER','Vancouver');
   INSERT INTO BASINS VALUES
-    ('WCI','I','WC INT');
+    ('WCI','I','WC INT','West-Central Interceptor');
   INSERT INTO BASINS VALUES
-    ('WHE','C','WHEELER');
+    ('WHE','C','WHEELER','Wheeler');
   INSERT INTO BASINS VALUES
-    ('WII','I','WILL INT');
+    ('WII','I','WILL INT','Willamette Interceptor');
   INSERT INTO BASINS VALUES
-    ('WOO','C','WOODS');
+    ('WOO','C','WOODS','Woods');
   INSERT INTO BASINS VALUES
-    ('WST','T','WST');
+    ('WST','T','WST','WSCSO Tunnel');
 
   TRUNCATE TABLE [SCIP].[dbo].[ACTIVITY_TYPES];
   SET IDENTITY_INSERT ACTIVITY_TYPES ON;

dae751dfbbfd1ed8e38e265c0a32eccbc73de290
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-03 13:32:25 -0700

    SCIPDatabase: Added hansen_area field to BASINS

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/BASINS.table.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/BASINS.table.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[BASINS] (
     [basin_id]      VARCHAR (3)  NOT NULL,
     [basin_type_id] CHAR (1)     NOT NULL,
+    [hansen_area]   VARCHAR (20) NULL,
     [name]          VARCHAR (50) NOT NULL
 );
 

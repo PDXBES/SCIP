4513f1714564d1b63a59ad5fa0c515ec204896a0
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-03 14:03:51 -0700

    SCIPDatabase: Added updating of districts during run of SP_ExposeData; added structural_grade to ASSETS

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/ASSETS.table.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/ASSETS.table.sql
@@ -4,9 +4,10 @@
     [diamWidth_inches]          FLOAT       NOT NULL,
     [height_inches]             FLOAT       NULL,
     [basin_id]                  VARCHAR (3) NOT NULL,
-	[district_id]               VARCHAR (4) NOT NULL,
+	  [district_id]               VARCHAR (4) NOT NULL,
     [last_inspection_date]      DATETIME    NULL,
     [last_root_management_date] DATETIME    NULL,
-    [last_cleaning_date]        DATETIME    NULL
+    [last_cleaning_date]        DATETIME    NULL,
+    [structural_grade]          INT         NULL
 );
 

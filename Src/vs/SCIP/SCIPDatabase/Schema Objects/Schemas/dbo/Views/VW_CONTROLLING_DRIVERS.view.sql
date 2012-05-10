5541d02363246583d1efcb1e88c289bbd54d2fc0
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-10 13:29:27 -0700

    SCIPDatabase: fixed VW_CONTROLLING_DRIVERS (was not getting all drivers before); added driver cost to VW_ALL_DRIVERS by adding VW_RANKED_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_CONTROLLING_DRIVERS.view.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_CONTROLLING_DRIVERS.view.sql
@@ -1,5 +1,5 @@
 ﻿CREATE VIEW [dbo].[VW_CONTROLLING_DRIVERS]
 	AS
   SELECT A.*
-  FROM VW_ALL_DRIVERS A INNER JOIN VW_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE B ON (A.compkey = B.compkey AND A.activity_type = B.activity_type AND A.frequency_years = B.min_frequency_years)
+  FROM VW_ALL_DRIVERS A INNER JOIN [VW_RANKED_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE] B ON (A.compkey = B.compkey AND A.activity_type = B.activity_type AND A.frequency_years = B.min_frequency_years AND A.driver_rank = B.min_driver_rank)
 

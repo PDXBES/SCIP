7ffd225fae3c89bb21780a7fe41adb5d27e04d87
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-09 21:27:21 -0700

    SCIPDatabase: Added rank field to DRIVER_TYPES to handle coincident drivers of the same activity type to help ensure that only one driver is controlling for an activity type

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/DRIVER_TYPES.table.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/DRIVER_TYPES.table.sql
@@ -5,6 +5,7 @@
     [description]             VARCHAR (100) NULL,
     [default_cost_per_ft]     MONEY         NOT NULL,
     [default_frequency_years] FLOAT         NOT NULL,
+    [rank]                    INT           NOT NULL DEFAULT 1,
     [alternative_id]          INT           NOT NULL
 );
 

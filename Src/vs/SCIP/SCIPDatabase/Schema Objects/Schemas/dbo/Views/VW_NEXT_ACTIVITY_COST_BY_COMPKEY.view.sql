9c39ad6a7295a5f90089d87a19f139d53e98dd57
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-16 22:34:02 -0700

    SCIPDatabase: fixed extraction of district id's; finished creationg of next activities table

---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_NEXT_ACTIVITY_COST_BY_COMPKEY.view.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[VW_NEXT_ACTIVITY_COST_BY_COMPKEY]
+	AS
+  SELECT 
+       [compkey]
+      ,[1] as inspection_cost
+      ,[2] as cleaning_cost
+      ,[3] as root_cost
+  FROM 
+    (SELECT compkey, activity_type_id, cost FROM VW_ALL_ACTIVITIES) PS
+    PIVOT (SUM(cost) for activity_type_id in ([1],[2],[3])) PVT

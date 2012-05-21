4912128df736e6bc08e63cbddecfe3b3a7557b85
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-21 07:20:32 -0700

    SCIPDatabase: Fixed misordered results UI: Fleshed out initial composition of UI

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_NEXT_ACTIVITY_COST_BY_COMPKEY.view.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_NEXT_ACTIVITY_COST_BY_COMPKEY.view.sql
@@ -3,8 +3,8 @@
   SELECT 
        [compkey]
       ,[1] as inspection_cost
-      ,[2] as cleaning_cost
-      ,[3] as root_cost
+      ,[2] as root_cost
+      ,[3] as cleaning_cost
   FROM 
     (SELECT compkey, activity_type_id, cost FROM VW_ALL_ACTIVITIES) PS
     PIVOT (SUM(cost) for activity_type_id in ([1],[2],[3])) PVT

f8bdfb13afd78b890a1da8cc5b1f5b0a3195b6b0
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-21 15:14:07 -0700

    SCIPDatabas: Added utility functions for ASSETS table; modified views to account for FN_ASSETS_TOTAL_COUNT rename

---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Tables/ASSETS.table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ASSETS] (
+    [COMPKEY]                   INT         NOT NULL,
+    [length_ft]                 INT         NOT NULL,
+    [diamWidth_inches]          FLOAT       NOT NULL,
+    [height_inches]             FLOAT       NULL,
+    [basin_id]                  VARCHAR (3) NOT NULL,
+	  [district_id]               VARCHAR (4) NOT NULL,
+    [unit_type]                  VARCHAR (10) NOT NULL,
+    [last_inspection_date]      DATETIME    NULL,
+    [last_root_management_date] DATETIME    NULL,
+    [last_cleaning_date]        DATETIME    NULL,
+    [structural_grade]          INT         NULL,
+    [asset_set_id]              INT         NOT NULL
+);
+
---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Views/VW_RANKED_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE.view.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[VW_RANKED_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE]
+	AS
+  SELECT A.[compkey]
+      ,A.[activity_type]
+      ,A.[min_frequency_years]
+      ,MIN(B.[driver_rank]) AS min_driver_rank
+  FROM [VW_MIN_DRIVER_FREQUENCY_BY_ACTIVITY_TYPE] A INNER JOIN [VW_ALL_DRIVERS] B ON (A.compkey = B.compkey AND A.activity_type = B.activity_type AND A.min_frequency_years = B.frequency_years)
+  GROUP BY A.activity_type, A.compkey, A.min_frequency_years

52ad8a27f3ac38db5d2e4478dc46e34c5e000659
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-24 12:16:51 -0700

    SCIPDatabase: Added stored procedure for counting compkeys affected by activities (grouped by activity and driver type) between a pair of years

---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_ACTIVITIES_COUNT_BY_DRIVER_TYPE_BETWEEN_YEARS.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[SP_ACTIVITIES_COUNT_BY_DRIVER_TYPE_BETWEEN_YEARS]
+	@beginYear int = 2012, 
+	@endYear int = 2017
+AS
+	SELECT A.activity_type, A.driver_type, count(*) AS compkey_count
+  FROM
+  (SELECT activity_type, driver_type, compkey
+  FROM VW_ALL_ACTIVITIES
+  WHERE fiscal_year BETWEEN @beginYear AND @endYear
+  GROUP BY activity_type, driver_type, compkey) AS A
+  GROUP BY A.activity_type, A.driver_type
+  ORDER BY A.activity_type, A.driver_type

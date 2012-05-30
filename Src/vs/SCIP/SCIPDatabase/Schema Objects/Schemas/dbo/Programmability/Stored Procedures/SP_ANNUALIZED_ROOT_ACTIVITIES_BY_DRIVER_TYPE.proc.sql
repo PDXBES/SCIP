465c979df09a19ca6da367652be17de16a58cc73
ISSACG <ISSACG@localhost>
2012-05-30 15:20:40 -0700

    Merge

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_ANNUALIZED_ROOT_ACTIVITIES_BY_DRIVER_TYPE.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_ANNUALIZED_ROOT_ACTIVITIES_BY_DRIVER_TYPE.proc.sql
@@ -2,7 +2,7 @@
 	@beginYear int = 2012, 
 	@endYear int = 2017
 AS
-	SELECT driver_type, AVG(cost) AS annualized_cost, AVG(length_ft) AS annualized_length_ft
+	SELECT driver_type, reporting_category, AVG(cost) AS annualized_cost, AVG(length_ft) AS annualized_length_ft
   FROM VW_ROOT_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
   WHERE fiscal_year BETWEEN @beginYear AND @endYear
-  GROUP BY driver_type
+  GROUP BY driver_type, reporting_category

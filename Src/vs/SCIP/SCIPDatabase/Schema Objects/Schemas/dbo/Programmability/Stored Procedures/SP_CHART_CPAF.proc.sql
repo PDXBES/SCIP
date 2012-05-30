df8990c539f5fcfb6c781f14d6c7a564dcf0390b
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-30 15:23:23 -0700

    SCIPDatabase: Added line chart stored procedures

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_CPAF.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_CPAF.proc.sql
@@ -1,6 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[SP_CHART_CPAF]
-	@param1 int = 0, 
-	@param2 int
+	@beginYear int = 2012, 
+	@endYear int = 2023
 AS
-	SELECT @param1, @param2
-RETURN 0
+  SELECT reporting_category, AVG(length_ft) AS length_ft
+  FROM VW_CLEANING_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
+  WHERE fiscal_year BETWEEN @beginYear AND @endYear
+  GROUP BY reporting_category
--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_IPTF.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_IPTF.proc.sql
@@ -3,6 +3,6 @@
 	@endYear int = 2023
 AS
   SELECT reporting_category, SUM(length_ft) AS length_ft
-  FROM VW_CLEANING_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
+  FROM VW_INSPECTION_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
   WHERE fiscal_year BETWEEN @beginYear AND @endYear
   GROUP BY reporting_category

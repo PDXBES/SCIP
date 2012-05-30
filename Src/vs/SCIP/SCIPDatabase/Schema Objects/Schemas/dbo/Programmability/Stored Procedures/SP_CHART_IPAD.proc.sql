8f6af1578a03f9cc3f2082bf7400358bed837801
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-30 15:23:23 -0700

    SCIPDatabase: Added line chart stored procedures

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_IPAD.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_IPAD.proc.sql
@@ -3,6 +3,6 @@
 	@endYear int = 2023
 AS
   SELECT reporting_category, AVG(cost) AS cost
-  FROM VW_CLEANING_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
+  FROM VW_INSPECTION_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR
   WHERE fiscal_year BETWEEN @beginYear AND @endYear
   GROUP BY reporting_category
---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_RLTD.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[SP_CHART_RLTD]
+	@beginYear int = 2012, 
+	@endYear int = 2023
+AS
+  SELECT fiscal_year, 
+      SUM([Root Control High]) AS [Root Control High],
+      SUM([Root Control High (>15 in)]) AS [Root Control High (>15 in)],
+      SUM([Root Control Medium]) AS [Root Control Medium], 
+      SUM([Root Control Medium (>15 in)]) AS [Root Control Medium]
+  FROM (SELECT * FROM VW_ROOT_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR WHERE fiscal_year BETWEEN 2012 AND 2023) PS
+  PIVOT
+  (SUM(cost) FOR reporting_category IN 
+    (
+      [Root Control High],
+      [Root Control High (>15 in)],
+      [Root Control Medium], 
+      [Root Control Medium (>15 in)]
+    )) AS PVT
+  GROUP BY fiscal_year
+  ORDER BY fiscal_year

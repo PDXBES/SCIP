32588012027ffd5a160e15b0c295907104ad6943
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-31 10:43:48 -0700

    SCIPDatabase: Added chart functions that work the same way as the stored procedures to account for CLS naming issue in report

---
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/FN_CHART_RLTF.function.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[FN_CHART_RLTF]
+(
+	@beginYear int, 
+	@endYear int
+)
+RETURNS TABLE 
+AS
+RETURN (
+  SELECT fiscal_year, 
+      ISNULL(SUM([Root Control High]), 0) AS [Root Control High],
+      ISNULL(SUM([Root Control High (>15 in)]), 0) AS [Root Control High (>15 in)],
+      ISNULL(SUM([Root Control Medium]), 0) AS [Root Control Medium], 
+      ISNULL(SUM([Root Control Medium (>15 in)]), 0) AS [Root Control Medium (>15 in)]
+  FROM (SELECT * FROM VW_ROOT_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR WHERE fiscal_year BETWEEN 2012 AND 2023) PS
+  PIVOT
+  (SUM(length_ft) FOR reporting_category IN 
+    (
+      [Root Control High],
+      [Root Control High (>15 in)],
+      [Root Control Medium], 
+      [Root Control Medium (>15 in)]
+    )) AS PVT
+  GROUP BY fiscal_year
+
+)
--- Src/vs/SCIP/UI/FormCreateCharts.Designer.cs
+++ Src/vs/SCIP/UI/FormCreateCharts.Designer.cs
@@ -28,11 +28,24 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.SuspendLayout();
+      // 
+      // FormCreateCharts
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(715, 427);
+      this.Name = "FormCreateCharts";
       this.Text = "FormCreateCharts";
+      this.Load += new System.EventHandler(this.FormCreateCharts_Load);
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+
+
+
   }
 }

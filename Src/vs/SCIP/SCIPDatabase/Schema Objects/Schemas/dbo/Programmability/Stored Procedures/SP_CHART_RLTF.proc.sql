28e74d5014b91b2b305da48b81b19299c6988486
Arnel Mandilag <Arnel Mandilag@localhost>
2012-05-31 06:25:48 -0700

    SCIPDatabase: fixed mislabeling of columns in root control line chart queries

--- Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_RLTF.proc.sql
+++ Src/vs/SCIP/SCIPDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SP_CHART_RLTF.proc.sql
@@ -6,7 +6,7 @@ AS
       ISNULL(SUM([Root Control High]), 0) AS [Root Control High],
       ISNULL(SUM([Root Control High (>15 in)]), 0) AS [Root Control High (>15 in)],
       ISNULL(SUM([Root Control Medium]), 0) AS [Root Control Medium], 
-      ISNULL(SUM([Root Control Medium (>15 in)]), 0) AS [Root Control Medium]
+      ISNULL(SUM([Root Control Medium (>15 in)]), 0) AS [Root Control Medium (>15 in)]
   FROM (SELECT * FROM VW_ROOT_ACTIVITIES_BY_DRIVER_TYPE_FISCAL_YEAR WHERE fiscal_year BETWEEN 2012 AND 2023) PS
   PIVOT
   (SUM(length_ft) FOR reporting_category IN 

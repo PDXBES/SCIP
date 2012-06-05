5c49ac02e0b6bf68afecc7d7b284fe80e441a328
Arnel Mandilag <Arnel Mandilag@localhost>
2012-06-05 08:55:15 -0700

    UI: More spiffing up; added HomePage

---
+++ Src/vs/SCIP/UI/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI
+{
+  public partial class HomePage : UI.ChildFormTemplate
+  {
+    public HomePage()
+    {
+      InitializeComponent();
+    }
+
+    private void btnGeneric_Click(object sender, EventArgs e)
+    {
+      AppForm.SwitchToTab((sender as Control).Tag as string);
+    }
+  }
+}

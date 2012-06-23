3c18701c08c4b8c8f8aa4b7eb973c5d028387b9c
Arnel Mandilag <Arnel Mandilag@localhost>
2012-06-23 01:14:27 -0700

    UI: Added some optimizations to running the SP_INSERT_ACTIVITIES_FOR_PROCESSING stored procedure in case user changes year parameters (but not alternative) and regens the charts

--- Src/vs/SCIP/UI/ChartsPage.cs
+++ Src/vs/SCIP/UI/ChartsPage.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Microsoft.Reporting.WinForms;
+using System.Data.SqlClient;
 
 namespace UI
 {
   public partial class ChartsPage : ChildFormTemplate
   {
+    bool insertedActivitiesForProcessing = false;
+
     const int chartTimeOutSeconds = 300;
     public ChartsPage()
     {
@@ -31,8 +34,36 @@ namespace UI
       SetProgressUIVisible(false);
     }
 
+    private void InsertActivitiesForProcessing()
+    {
+      var connection = new SqlConnection(SCIPUI.Default.ConnectionString);
+      try
+      {
+        Cursor = Cursors.WaitCursor;
+        var command = new SqlCommand("SP_INSERT_ACTIVITIES_FOR_PROCESSING", connection)
+        {
+          CommandType = CommandType.StoredProcedure
+        };
+        command.Parameters.Add("@alternative_id", SqlDbType.Int).Value = (int)cmbAlternative.Value;
+        command.CommandTimeout = chartTimeOutSeconds;
+        connection.Open();
+        command.ExecuteNonQuery();
+        insertedActivitiesForProcessing = true;
+      }
+      finally
+      {
+        connection.Close();
+        Cursor = Cursors.Default;
+      }
+    }
+
     private void RunCleaningReport()
     {
+      if (!insertedActivitiesForProcessing)
+      {
+        InsertActivitiesForProcessing();
+      }
+
       int beginYear = Convert.ToInt32(txtBeginYear.Text);
       int endYear = Convert.ToInt32(txtEndYear.Text);
 
@@ -42,12 +73,12 @@ namespace UI
       this.FN_CHART_CPAFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_CLTDTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_CLTFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
-      this.FN_CHART_CPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_CPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPTF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_CPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPAD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_CPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPAF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_CLTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CLTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_CLTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CLTF, beginYear, endYear, (int)comboAlternative.Value);
+      this.FN_CHART_CPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_CPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPTF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_CPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPAD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_CPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CPAF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_CLTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CLTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_CLTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_CLTF, beginYear, endYear, (int)cmbAlternative.Value);
 
       ReportParameter p1 = new ReportParameter("beginYear", txtBeginYear.Text);
       ReportParameter p2 = new ReportParameter("endYear", txtEndYear.Text);
@@ -74,6 +105,11 @@ namespace UI
 
     private void RunInspectionReport()
     {
+      if (!insertedActivitiesForProcessing)
+      {
+        InsertActivitiesForProcessing();
+      }
+
       int beginYear = Convert.ToInt32(txtBeginYear.Text);
       int endYear = Convert.ToInt32(txtEndYear.Text);
 
@@ -83,12 +119,12 @@ namespace UI
       this.FN_CHART_IPAFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_ILTDTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_ILTFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
-      this.FN_CHART_ILTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_ILTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_ILTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_ILTF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_IPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPAD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_IPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPAF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_IPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_IPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPTF, beginYear, endYear, (int)comboAlternative.Value);
+      this.FN_CHART_ILTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_ILTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_ILTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_ILTF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_IPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPAD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_IPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPAF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_IPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_IPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_IPTF, beginYear, endYear, (int)cmbAlternative.Value);
       this.rptvwInspection.RefreshReport();
 
       ReportParameter p1 = new ReportParameter("beginYear", txtBeginYear.Text);
@@ -116,6 +152,11 @@ namespace UI
 
     private void RunRootReport()
     {
+      if (!insertedActivitiesForProcessing)
+      {
+        InsertActivitiesForProcessing();
+      }
+
       int beginYear = Convert.ToInt32(txtBeginYear.Text);
       int endYear = Convert.ToInt32(txtEndYear.Text);
 
@@ -125,12 +166,12 @@ namespace UI
       this.FN_CHART_RPAFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_RLTDTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_RLTFTableAdapter.SelectCommandTimeOut = chartTimeOutSeconds;
-      this.FN_CHART_RPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_RPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPTF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_RPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPAD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_RPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPAF, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_RLTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RLTD, beginYear, endYear, (int)comboAlternative.Value);
-      this.FN_CHART_RLTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RLTF, beginYear, endYear, (int)comboAlternative.Value);
+      this.FN_CHART_RPTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_RPTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPTF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_RPADTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPAD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_RPAFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RPAF, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_RLTDTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RLTD, beginYear, endYear, (int)cmbAlternative.Value);
+      this.FN_CHART_RLTFTableAdapter.Fill(this.SCIPDataSet.FN_CHART_RLTF, beginYear, endYear, (int)cmbAlternative.Value);
 
       ReportParameter p1 = new ReportParameter("beginYear", txtBeginYear.Text);
       ReportParameter p2 = new ReportParameter("endYear", txtEndYear.Text);
@@ -179,20 +220,25 @@ namespace UI
 
     private void RunCoverageReport()
     {
+      if (!insertedActivitiesForProcessing)
+      {
+        InsertActivitiesForProcessing();
+      }
+
       int beginYear = Convert.ToInt32(txtBeginYear.Text);
       int endYear = Convert.ToInt32(txtEndYear.Text);
       this.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSCleaningTableAdapter.Fill(
-        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSCleaning, beginYear, endYear, (int)comboAlternative.Value);
+        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSCleaning, beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 17;
       Refresh();
 
       this.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSInspectionTableAdapter.Fill(
-        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSInspection, beginYear, endYear, (int)comboAlternative.Value);
+        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSInspection, beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 33;
       Refresh();
 
       this.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSRootTableAdapter.Fill(
-        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSRoot, beginYear, endYear, (int)comboAlternative.Value);
+        this.SCIPDataSet.FN_CHART_ACTIVITIES_COUNT_BETWEEN_YEARSRoot, beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 50;
       Refresh();
 
@@ -201,7 +247,7 @@ namespace UI
         SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSInspectionTableAdapter.Fill(
         this.SCIPDataSet.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSInspection,
-        beginYear, endYear, (int)comboAlternative.Value);
+        beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 67;
       Refresh();
 
@@ -209,7 +255,7 @@ namespace UI
         SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSCleaningTableAdapter.Fill(
         this.SCIPDataSet.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSCleaning,
-        beginYear, endYear, (int)comboAlternative.Value);
+        beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 83;
       Refresh();
 
@@ -217,7 +263,7 @@ namespace UI
         SelectCommandTimeOut = chartTimeOutSeconds;
       this.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSRootTableAdapter.Fill(
         this.SCIPDataSet.FN_CHART_PROGRESSIVE_ACTIVITIES_COUNT_BETWEEN_YEARSRoot,
-        beginYear, endYear, (int)comboAlternative.Value);
+        beginYear, endYear, (int)cmbAlternative.Value);
       progress.Value = 95;
       Refresh();
 
@@ -251,6 +297,11 @@ namespace UI
     {
       if ((int)txtEndYear.Value  < (int)txtBeginYear.Value)
         txtEndYear.Value = txtBeginYear.Value;
+    }
+
+    private void comboAlternative_ValueChanged(object sender, EventArgs e)
+    {
+      insertedActivitiesForProcessing = false;
     }
   }
 }
